2085df9c5001ff51a3467d7683a12b357e87ac41
Nafis Zaman <nafis.zaman-1@ou.edu>
2016-01-09 18:29:23 +0600

    Reworked wait_for_light function

--- src/botball_c.cpp
+++ src/botball_c.cpp
@@ -8,20 +8,6 @@
 
 #include <iostream>
 
-/*#include "kovan/button.h"
-#include "kovan/util.h"
-#include "kovan/display.h"
-#include "kovan/analog.h"
-#include "kovan/audio.h"
-#include "kovan/graphics.h"
-#include "kovan/thread.hpp"
-#include "kovan/general.h"
-#include "kovan/create.hpp"
-#include "kovan/compat.hpp"
-
-#include <cstdlib>
-#include <iostream>*/
-
 void colorbar(int i);
 void display_clear();
 
@@ -36,90 +22,106 @@ void shut_down_in(double s)
   s_instance->start();
 }
 
-// Reworked for flash suppression January 6, 2015
-// Graphics signal for waiting
-#define FLASHPAUSE 100  // msecs to pause to let a flash subside
-/* Assumption is that if light is detected by consecutive readings FLASHPAUSE apart, 
-   it's the start light, not a flash
-   NOTE: when tested with camera, the flash persisted < 0.1 sec
-*/
-#define THRESHOLD 60    // minimum discrimination between light and dark
-#define RED 255,0,0
-#define GREEN 0,255,0
+// Msecs to pause to let a flash subdue
+// Assumption is that if light is detected by consecutive readings FLASHPAUSE apart, 
+// it's the start light, not a flash.
+// NOTE: when tested with camera, the flash persisted < 0.1 sec
+#define FLASHPAUSE 100 
+
 void wait_for_light(int light_port)
 {
-  int xBut, OK=0, onval, offval, reading, cb=0, i; 
-  xBut=get_extra_buttons_visible(); // in case user has extra buttons visible
-  set_extra_buttons_visible(0);     //    turn them off
-  set_a_button_text("-");
+  static const int THRESHOLD = 60;
+  
+  // Disable extra buttons
+  const bool extraButtons = get_extra_buttons_visible();
+  set_extra_buttons_visible(0);
+  
+  set_a_button_text("VALUE");
   set_c_button_text("-");
-  while (!OK) {
-    set_b_button_text("Light is ON");
-    display_clear();
-    printf("CALIBRATE: sensor port %d\n", light_port);
-    printf("press ON when light is on\n");
-    while(b_button()==0){
-      msleep(100);
-      onval=analog(light_port); // sensor value when light is on
-      printf("light on value is = %d\n", onval);
+  
+  bool goodCalib = false;
+  while(!goodCalib) {
+    std::cout << "CALIBRATION (sensor port " << light_port << ")" << std::endl;
+    std::cout << "Press VALUE to see current value" << std::endl;
+    
+    // ON and OFF calibration
+    int calibVals[2] = {-1, -1};
+    const std::string calibModes[2] = {"ON", "OFF"};
+    for(int i = 0; i < 2; ++i) {
+      const std::string bText = std::string("Light is ") + calibModes[i];
+      set_b_button_text(bText.c_str());
+      std::cout << "Press " << calibModes[i] << " when light is " << calibModes[i] << std::endl;
+      while(b_button() == 0) {
+        // Print current value
+        if(a_button()) {
+          const int currVal = analog(light_port);
+          std::cout << "Current value: " << currVal << std::endl;
+          while(a_button()) {}
+        }
+        msleep(100);
+      }
+      calibVals[i] = analog(light_port);
+      std::cout << calibModes[i] << " value: " << calibVals[i] << std::endl;
+      beep();
+      while(b_button()) {}
     }
-    set_b_button_text("Light is OFF");
-    printf("light on value is = %d\n", onval);
-    msleep(200);
-    beep();
-    while (b_button()); // debounce B button
-    printf("press OFF when light is off\n");
-    while (b_button()==0) {
-      offval=analog(light_port);
-      printf("light off value is %d\n", offval);
-      msleep(100);
-    }
-    offval=analog(light_port); // sensor value when light is off
-    printf("light off value is = %d\n", offval);
-    msleep(200);
-    if ((offval-onval)>=THRESHOLD) { // bright = small values
-      OK=1;
-      printf("Good calibration!\n");
-      printf("Diff = %d: WAITING FOR LIGHTS ON\n", offval-onval);
-      graphics_open(240,25);
-      graphics_fill(255,255,255);
+    
+    const int &onVal = calibVals[0];
+    const int &offVal = calibVals[1];
+    // Good calibration (small value = bright)
+    if(offVal - onVal >= THRESHOLD) {
+      goodCalib = true;
+      std::cout << "Good calibration! (diff = " << (offVal - onVal) << ")" << std::endl;
+      std::cout << "WAITING FOR LIGHTS ON" << std::endl;
+      graphics_open(240, 25);
+      graphics_fill(255, 255, 255);
       graphics_update();
-      while (1) { // loop until light verified on
-        colorbar((i++)%74);
+      int cbNum = 0;
+      // Loop until light is on
+      for(;;) {
+        cbNum = (cbNum + 1) % 74;
+        colorbar(cbNum);
         msleep(20);
-        reading=analog(light_port);
-        printf("Current reading: %d\n", reading);
-        if ((reading-onval) < THRESHOLD) { // reading is low enough for light on
-          msleep(FLASHPAUSE); // pause
-          reading=analog(light_port);  // get second reading to rule out flash  
-           if ((reading-onval) < THRESHOLD) break; // if still low enough, light is on
+        int currVal = analog(light_port);
+        // Reading low enough for light on
+        if(currVal - onVal < THRESHOLD) {
+          // Do a second check to rule out flash
+          msleep(FLASHPAUSE);
+          currVal = analog(light_port); 
+          if((currVal - onVal) < THRESHOLD) {
+            std::cout << "Reading: " << currVal << std::endl;
+            std::cout << "*** LIGHTS ARE ON ***" << std::endl;
+            break;
+          }
         }
       }
       graphics_close();
-      printf("Reading: %4d *** LIGHTS ARE ON ***\n", reading);
     }
+    // Bad calibration
     else {
-      printf("BAD CALIBRATION\n");
-      if (offval<256) {
-        printf("Add shielding!!\n");
-        msleep(5000);
-      }
-      else {
-        printf("Aim sensor!!\n");
-        msleep(5000);
-      }
+      std::cout << "BAD CALIBRATION" << std::endl;
+      if(offVal < 256)
+        std::cout << "Try adding shielding!" << std::endl;
+      else
+        std::cout << "Try aiming sensor!" << std::endl;
+      msleep(5000);
     }
   }
-  set_a_button_text("A"); set_b_button_text("B"); set_c_button_text("C"); 
-  set_extra_buttons_visible(xBut); // in case user had extra buttons visible
+  
+  // Set buttons texts back
+  // TODO: Set texts back to their previous value, not defaults
+  set_a_button_text("A");
+  set_b_button_text("B");
+  set_c_button_text("C"); 
+  set_extra_buttons_visible(extraButtons);
 }
 
 void colorbar(int i) {
-  graphics_rectangle_fill(12,0,227,23,GREEN);
-  graphics_circle_fill(11,11,12,GREEN);
-  graphics_circle_fill(228,11,12,GREEN);
-  if (i<37) graphics_circle_fill(11+6*i,11,12,255,0,0);
-  else graphics_circle_fill(6+6*(74-i),11,12,255,0,0);
+  graphics_rectangle_fill(12, 0, 227, 23, 0, 255, 0);
+  graphics_circle_fill(11, 11, 12, 0, 255, 0);
+  graphics_circle_fill(228, 11, 12, 0, 255, 0);
+  if(i < 37) graphics_circle_fill(11 + 6*i, 11, 12, 255, 0, 0);
+  else graphics_circle_fill(6 + 6*(74-i), 11, 12, 255, 0, 0);
   graphics_update();
 }
 
